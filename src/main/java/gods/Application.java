e4e437c259210b465e39b2894caeb4474efb6238
Michal-Danihelka <Michal-Danihelka>
2022-10-17 07:01:21 +0000

    Initial commit

---
+++ src/main/java/gods/Application.java
@@ -0,0 +1,9 @@
+package gods;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
